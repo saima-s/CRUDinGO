418bb8d0e7ae7c9327700aa8578cd2442758bcd4
SaimaZopsmart <saima.sultana@zopsmart.com>
2021-01-19 23:30:55 +0530

    Test cases added

--- Crud_test.go
+++ Crud_test.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"encoding/json"
-	"github.com/gorilla/mux"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"reflect"
+
+	"github.com/gorilla/mux"
 
 	//"database/sql"
 	//"errors"
@@ -13,63 +16,65 @@ import (
 	//"reflect"
 	"testing"
 )
-//func TestGetCustomerByNameReturnsResult(t *testing.T) {
-//	var testCases = []struct {
-//		inp string
-//		out []customer
-//	}{
-//
-//		{"?name=CustomerA", []customer{{ID: 1, Name: "CustomerA", DOB: "10/10/2010", Age: 10, Addr: address{1, "Hyderabad", "Telanagana", "12", 1}}}},
-//		{"?name=CustomerB", []customer{{ID: 2, Name: "CustomerB", DOB: "10/10/2010",Age:10, Addr: address{2, "Patna", "Bihar", "121", 2}}}},
-//		{"?", []customer{customer{ID: 1, Name: "CustomerA", DOB: "10/10/2010", Age: 10, Addr: address{1, "Hyderabad", "Telanagana", "12", 1}},
-//			                     customer{ID: 2, Name: "CustomerB", DOB: "10/10/2010",Age:10, Addr: address{2, "Patna", "Bihar", "121", 2}}}},
-//		{"?name=xyz", []customer(nil)},
-//	}
-//	for ind := range testCases{
-//		req := httptest.NewRequest("GET", "http://localhost:8080/customer" +testCases[ind].inp,nil)
-//		w:=httptest.NewRecorder()
-//		GetCustomerByName(w, req)
-//		resp := w.Body.Bytes()
-//		var cust []customer
-//		json.Unmarshal(resp,&cust)
-//		fmt.Println("cust is ", cust)
-//		if w.Code != http.StatusOK || !reflect.DeepEqual(cust, testCases[ind].out){
-//			t.Errorf("Expected %T, %v Output %T, %v",testCases[ind].out, testCases[ind].out,cust, cust)
-//		}
-//
-//	}
-//	}
-//func TestGetCustomerByIdReturnsResult(t *testing.T) {
-//	var testCases = []struct {
-//		inp string
-//		out customer
-//	}{
-//
-//		{"1", customer{ID: 1, Name: "CustomerA", DOB: "10/10/2010", Age: 10,Addr: address{1, "Hyderabad", "Telanagana", "12", 1}}},
-//		{"2", customer{ID: 2, Name: "CustomerB", DOB: "10/10/2010",Age:10, Addr: address{2, "Patna", "Bihar", "121", 2}}},
-//		{"70", customer{}},
-//	}
-//	for ind := range testCases{
-//		req := httptest.NewRequest("GET", "http://localhost:8070/customer/",nil)
-//		//req := httptest.NewRequest(http.MethodGet, "http://localhost:8070/customer/{id}", nil)
-//		req = mux.SetURLVars(req, map[string]string{"id": testCases[ind].inp})
-//		w:=httptest.NewRecorder()
-//		GetCustomerById(w, req)
-//		resp := w.Result()
-//		body,err := ioutil.ReadAll(resp.Body)
-//		if err != nil{
-//
-//		}
-//		var cust customer
-//		json.Unmarshal(body,&cust)
-//		fmt.Printf("body is: %v",string(body))
-//
-//		if !reflect.DeepEqual(cust, testCases[ind].out){
-//			t.Errorf("Expected %v Output %v",testCases[ind].out,cust)
-//		}
-//	}
-//
-//}
+
+func TestGetCustomerByNameReturnsResult(t *testing.T) {
+	var testCases = []struct {
+		inp string
+		out []customer
+	}{
+
+		{"?name=CustomerA", []customer{{ID: 1, Name: "CustomerA", DOB: "10/10/2010", Age: 10, Addr: address{1, "Hyderabad", "Telanagana", "12", 1}}}},
+		{"?name=CustomerB", []customer{{ID: 2, Name: "CustomerB", DOB: "10/10/2010", Age: 10, Addr: address{2, "Patna", "Bihar", "121", 2}}}},
+		{"?", []customer{customer{ID: 1, Name: "CustomerA", DOB: "10/10/2010", Age: 10, Addr: address{1, "Hyderabad", "Telanagana", "12", 1}},
+			customer{ID: 2, Name: "CustomerB", DOB: "10/10/2010", Age: 10, Addr: address{2, "Patna", "Bihar", "121", 2}}}},
+		{"?name=xyz", []customer(nil)},
+	}
+	for ind := range testCases {
+		req := httptest.NewRequest("GET", "http://localhost:8080/customer"+testCases[ind].inp, nil)
+		w := httptest.NewRecorder()
+		GetCustomerByName(w, req)
+		resp := w.Body.Bytes()
+		var cust []customer
+		json.Unmarshal(resp, &cust)
+		fmt.Println("cust is ", cust)
+		if w.Code != http.StatusOK || !reflect.DeepEqual(cust, testCases[ind].out) {
+			t.Errorf("Expected %T, %v Output %T, %v", testCases[ind].out, testCases[ind].out, cust, cust)
+		}
+
+	}
+}
+func TestGetCustomerByIdReturnsResult(t *testing.T) {
+	var testCases = []struct {
+		inp string
+		out customer
+	}{
+
+		{"1", customer{ID: 1, Name: "CustomerA", DOB: "10/10/2010", Age: 10, Addr: address{1, "Hyderabad", "Telanagana", "12", 1}}},
+		{"2", customer{ID: 2, Name: "CustomerB", DOB: "10/10/2010", Age: 10, Addr: address{2, "Patna", "Bihar", "121", 2}}},
+		{"70", customer{}},
+	}
+	for ind := range testCases {
+		req := httptest.NewRequest("GET", "http://localhost:8070/customer/", nil)
+		//req := httptest.NewRequest(http.MethodGet, "http://localhost:8070/customer/{id}", nil)
+		req = mux.SetURLVars(req, map[string]string{"id": testCases[ind].inp})
+		w := httptest.NewRecorder()
+		GetCustomerById(w, req)
+		resp := w.Result()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+
+		}
+		var cust customer
+		json.Unmarshal(body, &cust)
+		fmt.Printf("body is: %v", string(body))
+
+		if !reflect.DeepEqual(cust, testCases[ind].out) {
+			t.Errorf("Expected %v Output %v", testCases[ind].out, cust)
+		}
+	}
+
+}
+
 //func TestCreateCustomerReturnsResult(t *testing.T) {
 //	var testCases = []struct {
 //		inp customer
@@ -122,24 +127,23 @@ func TestDeleteCustomerReturnsResult(t *testing.T) {
 	var testCases = []struct {
 		inp string
 		out customer
-
 	}{
 
-		{"9", customer{ID: 9, Name: "CustomerA", DOB: "10/10/2010", Age: 10,Addr: address{9, "Hyderabad", "Telanagana", "12", 9}}},
+		{"9", customer{ID: 9, Name: "CustomerA", DOB: "10/10/2010", Age: 10, Addr: address{9, "Hyderabad", "Telanagana", "12", 9}}},
 		//{"2", customer{ID: 2, Name: "CustomerB", DOB: "10/10/2010", Age: 10, Addr: address{2, "Patna", "Bihar", "121", 2}}},
 		//{"86", customer{}},
 	}
-	for ind := range testCases{
-		req := httptest.NewRequest(http.MethodDelete, "http://localhost:8070/customer/",nil)
+	for ind := range testCases {
+		req := httptest.NewRequest(http.MethodDelete, "http://localhost:8070/customer/", nil)
 		//req := httptest.NewRequest(http.MethodGet, "http://localhost:8070/customer/{id}", nil)
 		req = mux.SetURLVars(req, map[string]string{"id": testCases[ind].inp})
-		w:=httptest.NewRecorder()
+		w := httptest.NewRecorder()
 		DeleteCustomerById(w, req)
 		resp := w.Body.Bytes()
 		var cust customer
-		json.Unmarshal(resp,&cust)
-		if w.Code != http.StatusOK || !reflect.DeepEqual(cust, testCases[ind].out){
-			t.Errorf("Expected %v Output %v",testCases[ind].out,cust)
+		json.Unmarshal(resp, &cust)
+		if w.Code != http.StatusOK || !reflect.DeepEqual(cust, testCases[ind].out) {
+			t.Errorf("Expected %v Output %v", testCases[ind].out, cust)
 		}
 	}
 }
